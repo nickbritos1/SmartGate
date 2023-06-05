ec006ae30cb9ffb5edced29912017897ebf674ea
Nica Britos <nicanorbritos@gmail.com>
2023-06-05 01:35:08 -0300

    boton descubrir

---
+++ assets/scripts/app.js
@@ -0,0 +1,24 @@
+/*const botonDescubrir = document.querySelector('.botonDescubrir');
+let flecha;
+
+botonDescubrir.addEventListener("mouseenter", () => {
+    mostrarFlecha();
+});
+
+botonDescubrir.addEventListener("mouseleave", () => {
+    ocultarFlecha();
+});
+
+function mostrarFlecha() {
+    flecha = document.createElement("i");
+    flecha.className = 'fas fa-arrow-right flechaDescubrir';
+    botonDescubrir.appendChild(flecha);
+}
+
+function ocultarFlecha() {
+    if (flecha) {
+    flecha.remove();
+    flecha = null;
+    }
+}*/
+
